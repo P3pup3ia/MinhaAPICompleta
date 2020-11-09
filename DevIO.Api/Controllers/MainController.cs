4b52ed33eda1fe18595a0983f38092d8216e31ce
P3pup3ia <robert_sancts@hotmail.com>
2020-11-08 23:40:05 -0300

    Ajustando o banco

---
+++ DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,22 @@
+﻿using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+
+    [ApiController]
+   
+    public abstract class MainController : ControllerBase
+    {
+        //validação de notificações de erro
+        // validação de modestate
+        //validação da operação de negocios
+
+    }
+
+   
+
+}
